184f6ccd6f8be18648f1227e3d13624101ee5d26
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-10-29 11:00:37 +0200

    H,M,F

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return ( 
+        <div>
+            <header>
+                <h1>My Favourite Cities</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
