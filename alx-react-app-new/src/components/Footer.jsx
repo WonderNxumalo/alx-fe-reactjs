a9a46adf11b275ebea947229256c94312d7f8c5c
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-11-07 21:14:41 +0200

    styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div>
+            <footer style={{fontStyle: 'italic'}}>
+                <p>&copy; 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <div>
+            <main>
+                <p style={{fontWeight: 'bold', color: '#90a6ab'}}>I love to visit <span style={{color: 'blue'}}>New York</span>, <span style={{color: 'red'}}>Paris </span>, and <span style={{color: 'white'}}>Tokyo</span>.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
