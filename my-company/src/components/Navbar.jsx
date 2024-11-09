a308faab9e59850122d13f00a2b5de1800d48752
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-11-09 21:15:16 +0200

    backgroundColor

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar (){
     return (
-        <nav>
+        <nav /*style={{backgroundColor: 'black'}}*/>
             <ul className="navbar" style={{display: 'flex',  justifyContent: 'space-evenly'}}>
             <li><Link to="/">Home</Link></li>
             <li style={{marginLeft: 'auto'}}><Link to="/about">About</Link></li>
