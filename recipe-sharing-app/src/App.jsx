1e625f744fe9f75bc2d128ed28a4cc1d542597d1
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-11-12 19:12:25 +0200

    recipe app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import * as AddRecipeForm from './assets/components/AddRecipeForm'
+import * as RecipeList from './assets/components/RecipeList'
+
+function App() {
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  );
+};
+
+export default App;
