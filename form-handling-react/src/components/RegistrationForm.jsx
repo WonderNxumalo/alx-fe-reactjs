8b17af9485895123b2328a0d36f36784695e51e4
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-11-23 06:06:05 +0200

    Controlled Component

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,6 +1,90 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import validationSchema from './formikForm';
 
+/* 
+
+import { useState } from "react";
+
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const [errors, setErrors] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({...setFormData,[name]: value,});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Basic validation
+        const newErrors = {};
+        if (!formData.username) {newErrors.username = 'Username is required';}
+        if (!formData.email) {newErrors.email = 'Email is required';}
+        if (formData.password) {newErrors.password = 'Password is required';}
+
+        setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+        console.log('Form submitted: ', formData);
+    }
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input 
+                type="text" 
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                />
+                {errors.username && <p className="error">{errors.username}</p>}
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+            <div>
+            <label htmlFor="password">Password:</label>
+                <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                />
+                {errors.password && <p className="error">{errors.password}</p>}
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+
+};
+
+export default RegistrationForm;
+
+*/
+
 const FormikForm = () => {
     return (
       <Formik
