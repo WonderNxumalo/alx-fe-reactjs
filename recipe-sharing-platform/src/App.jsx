d15aae998047239b2158283bb1283c5783d8bce9
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-12-02 09:42:20 +0200

    Recipe sharing app

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-blue-500'>Test Page</h1>
+      <p className='text-base text-blue-300'>Zizo-colour blind</p>
+      <button className='border-blue bg-blue-500 text-white'>Click Me</button>
+    </>
+  )
+}
+
+export default App
