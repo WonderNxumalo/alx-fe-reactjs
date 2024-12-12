7dcb3c2d23b8223aa054800994caaaf3b945c76e
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2024-12-12 07:51:32 +0200

    GH user search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-red-500'>Test</h1>
+    </>
+  )
+}
+
+export default App
--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -7,13 +7,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   
   return (
-
-   
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path='/recipeform/' element={<AddRecipeForm />} />
+        <Route path='/recipeform' element={<AddRecipeForm />} />
       </Routes>
     </Router> 
   );
